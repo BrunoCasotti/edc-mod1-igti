9ea81ba67ea64c984edce3b666b44d95e57f7c5f
Bruno Oliveira Casotti <bruno.casotti@live.com>
2022-05-09 20:53:10 -0300

    primeiro commit

---
+++ variables.tf
@@ -0,0 +1,12 @@
+
+variable "base_bucket_name" {
+  default = "datalake-bruno-igti-edc-tf"
+}
+
+variable "ambiente" {
+  default = "producao"
+}
+
+variable "numero_conta" {
+  default = "727133573929"
+}
